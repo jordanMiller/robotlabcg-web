c71e1b40853b5250ae3a1f16a99a7726a02c270a
jordan miller <jordan@robotlabcg>
2016-09-29 22:27:27 -0500

    Creates a nonfunctioning placeholder site

--- app.js
+++ app.js
@@ -2,13 +2,28 @@
 
 var app = angular.module('app',[]);
 
-app.controller('LandingPageCtrl', ['$scope',function ($scope){
+app.controller('LandingPageCtrl', ['$scope','$http', '$log', function ($scope,$http,$log){
 	$scope.email;
 	$scope.showForm = true;
+	$scope.formError = false;
 
 	$scope.signup = function(){
 		alert($scope.email);
-		$scope.showForm = false;	
+		var data = {'email' : $scope.email};
+		var successCallback = function(response){
+
+		};
+
+		$http.post('./scripts/action.php',data).success(function (data,status){
+			if(data.success) {
+				$scope.showForm = false;
+				alert('did the thing');
+			}
+		}).error(function(data,status){
+			$log.error('There was an error submitting the email address');	
+			$scope.formError = true;
+		});
+
 	};
 
 }]);
--- scripts/action.php
+++ scripts/action.php
@@ -9,9 +9,9 @@ if(isset($_POST)) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
  
-    $email_to = "example@email.com";
+    $email_to = "jordan@robotlabcg.com";
  
-    $email_subject = "Form Enquiry";
+    $email_subject = "Email List Signup";
  
    
  
@@ -35,35 +35,11 @@ if(isset($_POST)) {
  
      
  
-    $firstName = $_POST['firstName'];
- 
-    $lastName = $_POST['lastName'];
+    $email = $_POST['email'];
  
     $email_from = $_POST['email'];
  
-    $telephone = $_POST['telephone'];
- 
-    $startDate = substr($_POST['startDate'], 0, strpos($_POST['startDate'], "T"));
-    $startDate = date('Y-m-d H:i:s', strtotime($startDate . ' + 1 day'));
-    $startDate = str_replace(' 00:00:00', '', $startDate);
-
-    $endDate = substr($_POST['endDate'], 0, strpos($_POST['endDate'], "T"));
-    $endDate = date('Y-m-d H:i:s', strtotime($endDate . ' + 1 day'));
-    $endDate = str_replace(' 00:00:00', '', $endDate);
-
-    $totalDays = $_POST['totalDays'];
-
-    $groupSize = $_POST['groupSize'];
-
-    $mealType = $_POST['mealType'];
-
-    $messageDetails = $_POST['message'];
-
-    $percentage = number_format($_POST['percentage'], 2, '.', ',');
-
-    $total = number_format($_POST['total'], 2, '.', ',');
- 
-    $email_message = "Form details below.\n\n";
+    $email_message = "Please add the below email to contact list.\n\n";
  
      
  
@@ -77,65 +53,39 @@ if(isset($_POST)) {
  
      
  
-    $email_message .= "First Name: ".clean_string($firstName)."\n";
- 
-    $email_message .= "Last Name: ".clean_string($lastName)."\n";
- 
-    $email_message .= "Email: ".clean_string($email_from)."\n";
- 
-    $email_message .= "Telephone: ".clean_string($telephone)."\n";
- 
-    $email_message .= "Start Date: ".clean_string($startDate)."\n";
-
-    $email_message .= "End Date: ".clean_string($endDate)."\n";
-
-    $email_message .= "Total Days: ".clean_string($totalDays)."\n";
-
-    $email_message .= "Group Size: ".clean_string($groupSize)."\n";
-
-    $email_message .= "Meal Type: ".clean_string($mealType)."\n";
-
-    $email_message .= "Message Details: ".clean_string($messageDetails)."\n";
-
-    $email_message .= "Discount: $".clean_string($percentage)."\n";
-
-    $email_message .= "Total: $".clean_string($total)."\n";
+    $email_message .= "Email Address: ".clean_string($email)."\n";
  
      
  
      
  
-// create email headers
+    // create email headers
  
-$headers = 'From: '.$email_from."\r\n".
+    $headers = 'From: '.$email_from."\r\n".
  
-'Reply-To: '.$email_from."\r\n" .
+    'Reply-To: '.$email_from."\r\n" .
  
-'X-Mailer: PHP/' . phpversion();
+    'X-Mailer: PHP/' . phpversion();
  
-/*----
-Un-comment to enable email sending
-----*/
-// mail($email_to, $email_subject, $email_message, $headers); 
- 
+    /*----
+    Un-comment to enable email sending
+    ----*/
+    // mail($email_to, $email_subject, $email_message, $headers); 
+  
 ?>
-{
-    <!-- console degugger check to see if the form submitted correctly -->
-    "success": true,
-} 
- 
-<?php
- 
-} else {
+{ 
 
-?>   
-{
-    "success": false
-} 
+"success": true
+
+}
+
+
+<?php
+} else {
+?>
+{ 'success' : false} 
 
 <?php
 }
 
-
- 
 ?>
